41ae3e23b60643d751cc60f68aaf099e1d5a527b
Wayne Chen <wayne.chen@mile.cloud>
2023-11-04 15:52:15 +0000

    add: full pipeline of product_catalog, detail-page-view and home-page-view

---
+++ includes/constants.js
@@ -0,0 +1,27 @@
+// Declare dataset location
+const product_catalog = "dataform_retail_api";
+const user_event = "dataform_retail_api";
+
+// Create D-1 date in format YYYYMMDD
+const yesterday = new Date();
+yesterday.setDate(yesterday.getDate() - 1);
+const yesterday_no_separation = yesterday.toISOString().split('T')[0].replace(/-/g, '');
+
+const today = new Date();
+const today_dash_separation = today.toISOString().split('T')[0];
+
+// Define tables with table suffix for product_catalog
+const DAILY_RAW_PRODUCT_TABLE = "`kkday-retail-ai-poc." + product_catalog + ".raw_product_catalog_" + yesterday_no_separation + "`";
+const DAILY_RAW_WIFI_SIM_TABLE = "`kkday-retail-ai-poc." + product_catalog + ".raw_wifi_" + yesterday_no_separation + "`";
+
+// Define tables with table suffix for user_event
+const DAILY_USER_EVENT_TABLE = "`kkday-retail-ai-poc." + user_event + ".action_" + yesterday_no_separation + "`";
+const USER_EVENT_FILES = "'gs://kkday-retail-ai-poc-data/D/action/log_date=" + today_dash_separation + "/*.csv'"; // note that the D-1 data is in a D day folder
+
+// Export variables
+module.exports = {
+    DAILY_RAW_WIFI_SIM_TABLE,
+    DAILY_RAW_PRODUCT_TABLE,
+    DAILY_USER_EVENT_TABLE,
+    USER_EVENT_FILES
+};
